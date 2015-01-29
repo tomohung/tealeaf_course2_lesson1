1c1c05812554aeda5872f3361935580454c1fbac
Tomo Hung <tomohung@gmail.com>
2015-01-29 09:10:48 +0000

    add many association with post and category

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150127225820) do
+ActiveRecord::Schema.define(version: 20150129084146) do
+
+  create_table "categories", force: true do |t|
+    t.string "name"
+  end
 
   create_table "comments", force: true do |t|
     t.text    "body"
     t.integer "user_id"
     t.integer "post_id"
+  end
+
+  create_table "post_categories", force: true do |t|
+    t.integer "post_id"
+    t.integer "category_id"
   end
 
   create_table "posts", force: true do |t|

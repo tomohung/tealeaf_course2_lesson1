b1dc69652d6a9fc11928cf6ce12b01fb075c3609
Tomo Hung <tomohung@gmail.com>
2015-02-04 06:38:12 +0800

    add routes, views for new

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,34 @@
 class PostsController < ApplicationController
+  
+before_action :set_post, only: [:show, :edit, :update]
+
   def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def show
 
   end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def create
+
+  end
+
+private
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
 end

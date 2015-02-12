250cb13019eed90ce944b9f53778965f8d6711ce
Tomo Hung <tomohung@gmail.com>
2015-02-12 08:07:20 +0000

    fix views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
-  helper_method :current_user, :logged_in?
+  helper_method :current_user, :logged_in?, :created_by?
   
   def logged_in?
     !!current_user
@@ -13,4 +13,7 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
+  def created_by?(post)
+    post.creator == current_user
+  end
 end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,7 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.build(comment_param)
-    
+    @comment.creator = current_user
     if @comment.save
       flash[:notice] = "Comment has been post."
       redirect_to post_path(params[:post_id])

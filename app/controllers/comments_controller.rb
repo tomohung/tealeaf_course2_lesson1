07fbd8a323be24ec960bf9f5d0a3937b73108e8b
Tomo Hung <tomohung@gmail.com>
2015-02-13 02:06:46 +0000

    fix blowup when blank comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,10 +4,12 @@ class CommentsController < ApplicationController
     @post = Post.find(params[:post_id])
     @comment = @post.comments.build(comment_param)
     @comment.creator = current_user
+    
     if @comment.save
       flash[:notice] = "Comment has been post."
       redirect_to post_path(params[:post_id])
     else      
+      @post.comments.delete(@comment)
       render 'posts/show'
     end
   end

f6f3704bf51c99c898d89f7bfd245b30555c7ce7
Tomo Hung <tomohung@gmail.com>
2015-02-14 16:36:05 +0800

    fix blow up when database is empty

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,10 @@ class ApplicationController < ActionController::Base
   end
   
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    if session[:user_id] && !User.exists?(session[:user_id])
+      session[:user_id] = nil
+    end
+    @current_user ||= User.find(session[:user_id]) if session[:user_id] 
   end
 
   def created_by?(post)
